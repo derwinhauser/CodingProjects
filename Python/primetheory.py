6ea94e7e0e75447a94cc0fef69b083808244def1
derwinhauser <derwinhauser08@gmail.com>
2025-09-27 10:00:23 -0400

    program now prints out every even number with every prime number pair that equates to it

--- Python/primetheory.py
+++ Python/primetheory.py
@@ -6,19 +6,26 @@ not_prime = []
 equations = []
 max_int = 100
 
-for i in range(2, max_int-1):
+# spearates prime and non-prime numbers
+for i in range(2, max_int): 
     tracker = 1
-    for y in range(2, i-1):
+    for y in range(2, i):
         tracker = tracker*(i%y)
     if tracker == 0:
         not_prime.append(i)
     else: prime.append(i)
 
-for int in range(4, 101, 2):
+for int in range(4, max_int, 2):
     for number in prime:
         ans = int - number
         if ans in prime:
             equations.append([int, ans, number])
         else: continue
 
-print(equations)
+    
+for even in range(4, max_int, 2):
+    print(f'{even}: ', end="")
+    for equation in equations:       
+        if even == equation[0]:
+            print(f'{equation[1]} + {equation[2]}', end='|')
+    print('')
