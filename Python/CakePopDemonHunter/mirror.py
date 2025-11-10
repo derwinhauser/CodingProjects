b3ec108b188676c87e3e5f7b5ba18b06e3809b70
derwinhauser <derwinhauser08@gmail.com>
2025-11-10 10:59:21 -0500

    calculated the change in gray display frame-by-frame and display the difference on screen

--- Python/CakePopDemonHunter/mirror.py
+++ Python/CakePopDemonHunter/mirror.py
@@ -8,7 +8,7 @@ cap = cv.VideoCapture(0)
 if not cap.isOpened():
     print("Cannot open camera")
     exit()
-
+first = True
 while True:
     #Capture frame-by-frame
     ret, frame = cap.read()
@@ -24,18 +24,34 @@ while True:
 
     # Our operations on the frame come here
     gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
-
+    if first:
+        prev_gray=gray
+        first=False
+    # calculate change in gray
+    delta = cv.absdiff(gray, prev_gray).sum()
+    
     # Display Text
+    cv.putText(frame,str(delta),(50,300),
+        cv.FONT_HERSHEY_SIMPLEX,
+        1, # font_scale
+        (255,255,255), # color
+        2)  # Thickness
+
     cv.putText(frame,"Hello",(50,100),
-    cv.FONT_HERSHEY_SIMPLEX,
-    1, # font_scale
-    (100,0,100), # color
-    2)  # Thickness
+        cv.FONT_HERSHEY_SIMPLEX,
+        1, # font_scale
+        (100,0,100), # color
+        2)  # Thickness
 
     # Display the resulting frame
 
     # cv.imshow('frame', gray)
     cv.imshow('frame', frame)
+
+    # save previous frame
+    prev_gray = gray
+
+    # see if user wants to quit
     if cv.waitKey(1) == ord('q'):
         break
 
