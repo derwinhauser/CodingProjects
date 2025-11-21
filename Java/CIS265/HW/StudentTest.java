b498d82a9c2718d61eaa7b03cdb7181c5fbd8473
derwinhauser <derwinhauser08@gmail.com>
2025-11-21 12:43:33 -0500

    added comments

--- Java/CIS265/HW/StudentTest.java
+++ Java/CIS265/HW/StudentTest.java
@@ -2,20 +2,27 @@ import java.util.ArrayList;
 
 public class StudentTest{
     public static void main(String[] args){
+        // Variables
         ArrayList <Student> students = new ArrayList <Student>();
         Student s1 = new Student("John", 3.68);
         Student s2 = new Student("Mary", 4.00);
         Student s3 = new Student("Sam", 3.5);
         double tempgpa = 0.0;
+
+        //test introduce function
         s1.introduce();
+        // add students to ArrayList
         students.add(s1);
         students.add(s2);
         students.add(s3);
+
+        //Calculate avg
         for (int i=0; i<students.size(); i++){
             Student temp = students.get(i);
             tempgpa = tempgpa+temp.getGpa();
         }
         double avg = tempgpa/students.size();
+        // print avg
         System.out.println("The average GPA of the students is: " + avg);
     }
 }
