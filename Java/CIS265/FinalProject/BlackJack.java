005b3e2613beff38dc65a5d35b05f3c1f6eeefa7
derwinhauser <derwinhauser08@gmail.com>
2025-11-24 19:51:04 -0500

    fixed temp deck removal

--- Java/CIS265/FinalProject/BlackJack.java
+++ Java/CIS265/FinalProject/BlackJack.java
@@ -10,19 +10,25 @@ import java.util.ArrayList;
 public class BlackJack{
 
     public static void main(String[] args){
-        Deck shoe = new Deck(6);
+        Deck shoe = new Deck(1);
         Deck discardTray = new Deck(0);
         int runningCount = 0;
         shoe.printShoeSize();
+        System.out.println("Shoe: ");
+        shoe.printDeck();
+        System.out.println("Discard Tray:");
         discardTray.printDeck();
         runningCount = discardTray.getRunningCount();
         System.out.println("Running count: " + runningCount);
         for (int i=0; i<20; i++){
-            Card tempCard = shoe.getCard(i);
-            shoe.removeCard(i);
+            Card tempCard = shoe.getCard(0);
+            shoe.removeCard(0);
             discardTray.addCard(tempCard);
         }
         shoe.printShoeSize();
+        System.out.println("Shoe: ");
+        shoe.printDeck();
+        System.out.println("Discard Tray:");
         discardTray.printDeck();
         runningCount = discardTray.getRunningCount();
         System.out.println("Running count: " + runningCount);
