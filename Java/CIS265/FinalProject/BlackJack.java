e8075e5aad91b09a583c714c01d9b4af6b8ab5c1
derwinhauser <derwinhauser08@gmail.com>
2025-11-30 21:04:52 -0500

    Added function to clear blackjackResults.csv before running

--- Java/CIS265/FinalProject/BlackJack.java
+++ Java/CIS265/FinalProject/BlackJack.java
@@ -1,8 +1,10 @@
 import java.io.IOException;
-
+import java.io.FileWriter;
 public class BlackJack{
     public static void main(String args[]) throws IOException{
+        FileWriter writer = new FileWriter("blackjackResults.csv", false);
         Table table = new Table(4);
         table.playShoe(100);
+        writer.close();
     }
 }
