1c242e573e591a8b0def1a0ee206bb449c5e6e73
derwinhauser <derwinhauser08@gmail.com>
2025-11-30 20:58:59 -0500

    moved writefile to beginning of playShoe script as blackjacks skipped the function

--- Java/CIS265/FinalProject/BlackJack.java
+++ Java/CIS265/FinalProject/BlackJack.java
@@ -3,6 +3,6 @@ import java.io.IOException;
 public class BlackJack{
     public static void main(String args[]) throws IOException{
         Table table = new Table(4);
-        table.playShoe(10000);
+        table.playShoe(100);
     }
 }
